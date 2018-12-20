b1d5b06034bbb1b9d74e22b64cd7bec9b32f34df
Christopher Hahn <chris@creativefuse.org>
2018-12-19 21:33:19 -0500

    Build Better Button Transitions with Font Awesome SVGs

--- themes/fuse/resources/views/fragments/components/_c-btn.php
+++ themes/fuse/resources/views/fragments/components/_c-btn.php
@@ -33,7 +33,17 @@ use Fuse\AssetHandler;
 
     <?php if( $type === 'primary' ){ ?>
 
-        <span class="c-btn__icon"></span>
+        <span class="c-btn__icon">
+            <svg aria-hidden="true" data-prefix="fal" data-icon="angle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="currentColor" d="M166.9 264.5l-117.8 116c-4.7 4.7-12.3 4.7-17 0l-7.1-7.1c-4.7-4.7-4.7-12.3 0-17L127.3 256 25.1 155.6c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0l117.8 116c4.6 4.7 4.6 12.3-.1 17z"></path></svg>
+        </span>
+
+    <?php } ?>
+
+    <?php if( $type === 'tertiary' ){ ?>
+
+        <span class="c-btn__icon">
+        <svg aria-hidden="true" data-prefix="fal" data-icon="long-arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg>
+        </span>
 
     <?php } ?>
 
--- themes/fuse/resources/views/fragments/sections/home/_intro.php
+++ themes/fuse/resources/views/fragments/sections/home/_intro.php
@@ -26,10 +26,8 @@ use function Fuse\Controllers\render as render;
 
             <div class="o-content-block__title">
                 <h3 class="f-hs--3xl">
-                    <span class="f-hw--b u-c--gray--d">Knack</span>
-                    <span class="f-hw--l u-c--gray--m">Creative</span>
-                    <span class="f-hw--b u-c--purple">.</span>
-                <h3>
+                    <span class="f-hw--b u-c--gray--d">Knack</span> <span class="f-hw--l u-c--gray--m">Creative</span><span class="f-hw--b u-c--purple">.</span>
+                </h3>
             </div>
 
             <div class="o-content-block__copy">
