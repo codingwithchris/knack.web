4438924003bb792bb033bf16ffc3d39265626154
Christopher Hahn <chris@creativefuse.org>
2019-05-05 14:34:50 -0400

    Change Medium Gray to Dark Gray in Client Section

--- themes/fuse/resources/views/fragments/sections/single-project/_client.php
+++ themes/fuse/resources/views/fragments/sections/single-project/_client.php
@@ -23,7 +23,7 @@ use function Fuse\Controllers\render as render;
 
 	<div class="p-project__client-info">
 
-		<h3 class="p-project__client-name f-hw--b f-hs--m u-c--gray--m"><?= esc_html( $data['name'] ); ?></h3>
+		<h3 class="p-project__client-name f-hw--b f-hs--m u-c--gray--d"><?= esc_html( $data['name'] ); ?></h3>
 
 		<?php if( isset( $data['website'] ) ){ ?>
 			<a class="p-project__client-website f-b--xs" href="<?= esc_url( $data['website'] ); ?>" target="_blank" rel="noopener">
@@ -33,17 +33,17 @@ use function Fuse\Controllers\render as render;
 
 	</div>
 
-	<div class="p-project__client-description u-c--gray--m">
+	<div class="p-project__client-description u-c--gray--d">
 			<p class="f-b--s"><?= esc_html( $data['description'] ); ?></p>
 	</div>
 
 	<div class="p-project__client-industries p-project__meta">
 
-		<div class="p-project__meta__title u-c--gray--m u-mbn">
+		<div class="p-project__meta__title u-c--gray--d u-mbn">
 			<h3 class="f-hw--b f-hs--s"><?= esc_html( 'Client Industries' ); ?></h3>
 		</div>
 
-		<div class="p-project__meta__list u-mbn u-c--gray--m">
+		<div class="p-project__meta__list u-mbn u-c--gray--d">
 
 			<p class="p-project__meta__item"><?= $data['industries']; ?></p>
 
