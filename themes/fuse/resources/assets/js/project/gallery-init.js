6b236abf71e37d94e7248512a19c4e49f9a0dbab
Christopher Hahn <chris@creativefuse.org>
2019-06-18 19:57:54 -0400

    Fix Photo Gallery Opening/Clsoing Index Glitch
    - Had to re-write the thumbnail function to take an index

--- themes/fuse/resources/assets/js/project/gallery-init.js
+++ themes/fuse/resources/assets/js/project/gallery-init.js
@@ -6,7 +6,7 @@ import * as PhotoSwipeUI_Default from './photoswipe/photoswipe-ui-default' // es
  */
 
 const photos = document.querySelectorAll( '.js-lightbox' );
-let galleryData = []; // eslint-disable-line
+const galleryData = [];
 
 /**
  * Get the position of an element in the page and return the values needed by
@@ -28,24 +28,46 @@ const getElementPagePosition = ( element ) => {
 
 }
 
+/**
+ *
+ * @param {*} index
+ * @param {*} photo
+ */
 const openPhotoSwipe = ( index, photo ) => {
 
+    // The element we are injecting Photoswipe into
     const photoSwipeElement = document.querySelectorAll( '.pswp' )[0];
-    const image = photo.querySelector( '.c-progressive__image' );
 
     // define options for PhotoSwipe Gallery
+    // https://photoswipe.com/documentation/options.html
     const options = {
         index,
         showHideOpacity: false,
-        getThumbBoundsFn: () => getElementPagePosition( image ),
+
+        // Must take index, find the proper image, and return the element coordinates as defined in the options
+        getThumbBoundsFn: ( imageIndex ) => {
+
+            const image = document.querySelectorAll( '.c-gallery--photo .c-progressive__image' )[imageIndex];
+
+            return getElementPagePosition( image )
+
+        },
+        captionEl: false,
+        counterEl: false,
+        shareEl: false,
+        zoomEl: false,
     };
 
+    // Build and init the gallery with our defined options
     const gallery = new PhotoSwipe( photoSwipeElement, PhotoSwipeUI_Default, galleryData, options );
     gallery.init();
 
 }
 
 
+/**
+ *
+ */
 const handlePhotoClick = ( photo, index, event ) => {
 
     if( ! photo ) return false;
@@ -54,6 +76,9 @@ const handlePhotoClick = ( photo, index, event ) => {
 
 }
 
+/**
+ *
+ */
 photos.forEach(( photo, index ) => {
 
     photo.addEventListener( 'click', ( event ) => {
@@ -65,6 +90,9 @@ photos.forEach(( photo, index ) => {
 
 });
 
+/**
+ *
+ */
 photos.forEach(( photo, index ) => {
 
     const size = photo.getAttribute( 'data-size' ).split( 'x' );
