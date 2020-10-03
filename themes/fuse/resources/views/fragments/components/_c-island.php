227513b46b79742d67ea7cae1baf17147f79a667
Chris Hahn <chris+digital@chrishahn.me>
2020-10-03 15:44:41 -0400

    Update Ox Blood to Dark Blue

--- themes/fuse/resources/views/fragments/components/_c-island.php
+++ themes/fuse/resources/views/fragments/components/_c-island.php
@@ -17,7 +17,7 @@ use function Fuse\Controllers\render as render;
 
 $overlay_type = $data['type'] == 'light'
                 ? 'white--85'
-                : 'ox-blood--85';
+                : 'dark-blue--85';
 
 
 $overlay_data = [
--- themes/fuse/resources/views/fragments/sections/single-project/_info.php
+++ themes/fuse/resources/views/fragments/sections/single-project/_info.php
@@ -41,11 +41,11 @@ use function Fuse\Controllers\render as render;
 
 	<div class="p-project__client-industries p-project__meta">
 
-		<div class="p-project__meta__title u-c--ox-blood">
+		<div class="p-project__meta__title u-c--dark-blue">
 			<h3 class="f-hw--b f-hs--s"><?= esc_html( _n( 'Project Category', 'Project Categories', $data['category_count'] ) ); ?></h3>
 		</div>
 
-		<div class="p-project__meta__list u-c--ox-blood">
+		<div class="p-project__meta__list u-c--dark-blue">
 
 			<p class="p-project__meta__item"><?= $data['categories']; ?></p>
 
